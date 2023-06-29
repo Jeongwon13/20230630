9964a23ee8b436d281593115e590d3699028fb8d
Jeongwon Choi <chlwjddnjs17@naver.com>
2023-06-30 00:38:28 +0900

    인프런 자바 알고리즘

---
+++ src/pkg1/Main.java
@@ -0,0 +1,40 @@
+package pkg1;
+
+import java.util.HashMap;
+import java.util.*;
+
+public class Main {
+
+	public String solution(String s1, String s2) {
+		
+		String answer = "YES";
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		for(char x : s1.toCharArray()) {
+			map.put(x, map.getOrDefault(x, 0)+1);
+		}
+		
+		for(char x : s2.toCharArray()) {
+			if(!map.containsKey(x) || map.get(x)==0) return "NO";
+			map.put(x, map.get(x)-1);
+		}
+		
+		 
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		String str2 = sc.next();
+		
+		System.out.print(T.solution(str, str2));
+		
+	}
+
+}
